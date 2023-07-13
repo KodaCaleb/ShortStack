25343c72d8f2daf5306a273cd164aca6a5e508da
KodaCaleb <122597981+KodaCaleb@users.noreply.github.com>
2023-07-13 15:06:03 -0500

    added tailwind to project

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from '@firebase/firestore'; 
+import { getStorage } from '@firebase/storage';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -16,4 +17,10 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+
+// Initialize Firestore and get a reference to the service
 export const firestore = getFirestore(app);
+
+// Initialize Cloud Storage and get a reference to the service
+export const storage = getStorage(app);

eabbe2cffdf727a1a593f5576d7e1a6bda95a416
csullins <camilesullins@gmail.com>
2023-07-13 15:11:03 -0500

    tap to play logic

--- src/Video.js
+++ src/Video.js
@@ -1,15 +1,28 @@
+import { useRef, useState } from "react";
 import "./Video.css";
 import sampleVid1 from "./assets/sampleVid1.mp4";
 
 function Video() {
+  const [playing, setPlaying] = useState(false);
+  const videoRef = useRef(null);
+  const onVideoPress = () => {
+    if (playing) {
+      videoRef.current.pause();
+      setPlaying(false);
+    } else {
+      videoRef.current.play();
+      setPlaying(true);
+    }
+  };
+
   return (
     <div className="videoContainer">
       <video 
       className="videoPlayer" 
       src={sampleVid1} 
-      autoPlay 
+      ref={videoRef}
+      onClick={onVideoPress} 
       loop 
-      controls
       ></video>
       {/* <VideoFooter />*/}
       {/* <VideoSideBar /> */}

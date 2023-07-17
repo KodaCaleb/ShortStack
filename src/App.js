05b604c6338956cd3eaacc64cf41dc4ba19c8975
Jstanion <123262263+Jstanion@users.noreply.github.com>
2023-07-16 19:10:51 -0500

    created SVG icon for dropdown menu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 import Home from "./pages/Home";
 import Video from "./components/VideoContainer/video";
+import LoginModal from "./components/Modals/login"
+import SignUpModal from "./components/Modals/signUp"
 
 function App() {
   return (
     <div className="app">
       <Home />
+      <LoginModal />
+      <SignUpModal />
       <div className="app_videos">
         <Video />
         <Video />

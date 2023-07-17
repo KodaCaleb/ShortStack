972f8de334e413ec30e0682125ed715d48a24ecb
Joaquin Salazar <joaqsala@gmail.com>
2023-07-16 19:58:11 -0500

    added layouts to image and bio in profile.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import "./App.css";
 import Home from "./pages/Home";
 import Video from "./components/VideoContainer/video";
-import SignUpModal from "./components/Modals/signUp"
-import UserProfileHeading from "./components/Profile/profile"
-import Navbar from "./components/Header/navBar";
-
 function App() {
   return (
     <div className="app">
-      <UserProfileHeading />
-      {/* <div className="app_videos">
+      <Home />
+      <div className="app_videos">
         <Video />
         <Video />
         <Video />
         <Video />
-      </div> */}
+      </div>
     </div>
   );
 }
-
 export default App;
--- src/components/Profile/profile.js
+++ src/components/Profile/profile.js
@@ -2,52 +2,95 @@ import React, { useState } from "react";
 import { FaPencilAlt } from "react-icons/fa";
 
 export default function UserProfileHeading() {
-    const [isImageHovered, setIsImageHovered] = useState(false);
-    const [isUsernameHovered, setIsUsernameHovered] = useState(false);
-    const [isUsernameEditable, setIsUsernameEditable] = useState(false);
+  const [isImageHovered, setIsImageHovered] = useState(false);
+  const [isImageEditable, setIsImageEditable] = useState(false);
+  const [isUsernameHovered, setIsUsernameHovered] = useState(false);
+  const [isUsernameEditable, setIsUsernameEditable] = useState(false);
+  const [isBioHovered, setIsBioHovered] = useState(false);
+  const [isBioEditable, setIsBioEditable] = useState(false);
 
-    const [username, setUsername] = useState("username");
+  const [image, setImage] = useState(process.env.PUBLIC_URL + '/pancakeholder.img.png')
+  const [username, setUsername] = useState("Username");
+  const [bioInfo, setBioInfo] = useState("Here for the lulz");
 
-    const handleImageMouseEnter = () => {
-        setIsImageHovered(true);
-    };
+  const handleImageMouseEnter = () => {
+    setIsImageHovered(true);
+  };
 
-    const handleImageMouseLeave = () => {
-        setIsImageHovered(false);
-    };
+  const handleImageMouseLeave = () => {
+    setIsImageHovered(false);
+  };
 
-    const handleUsernameMouseEnter = () => {
-        setIsUsernameHovered(true);
-    }
+  const handleImageClick = (e) => {
+    e.preventDefault();
+    setIsImageEditable(!isImageEditable);
+  };
 
-    const handleUsernameMouseLeave = () => {
-        setIsUsernameHovered(false);
-    }
+  const handleImageUpload = (e) => {
+    const file = e.target.files[0];
+    // TODO add code to handle saving image and to use setImage useState
+  };
 
-    const handleUsernameClick = () => {
-        setIsUsernameEditable(true);
-      };
+  const handleUsernameMouseEnter = () => {
+    setIsUsernameHovered(true);
+  }
 
-    return (
-        <>
-            <div className="flex md:flex-row justify-center md:justify-between w-full md:w-1/2 border-solid border">
-                <div className="self-start rounded-full overflow-hidden flex items-center justify-center text-white px-4 border-solid border max-w-[200px] max-h-[200px] relative" onMouseEnter={handleImageMouseEnter}
-                    onMouseLeave={handleImageMouseLeave}>
-                    <img src={""} alt="self" className="w-full h-full object-cover" />
-                    {isImageHovered && (
-                        <div className="absolute top-2 right-2">
-                            <FaPencilAlt className="text-white text-xl" />
-                        </div>
-                    )}
-                </div>
-                <div className="flex flex-col justify-start pl-2 pr-4 md:pl-4 border-solid border">
-                    {/* stack of username and bio */}
-                    <div className="text-white border-solid border relative"
-                        onMouseEnter={handleUsernameMouseEnter}
-                        onMouseLeave={handleUsernameMouseLeave}
-                        onClick={handleUsernameClick}
-                    >
-                        {isUsernameEditable ? (
+  const handleUsernameMouseLeave = () => {
+    setIsUsernameHovered(false);
+  }
+
+  const handleUsernameClick = () => {
+    setIsUsernameEditable(!isUsernameEditable);
+  };
+
+  const handleBioMouseEnter = () => {
+    setIsBioHovered(true);
+  }
+
+  const handleBioMouseLeave = () => {
+    setIsBioHovered(false);
+  }
+
+  const handleBioClick = () => {
+    setIsBioEditable(!isBioEditable);
+  };
+
+  return (
+    <>
+      <div className="flex md:flex-row justify-center md:justify-between w-full mx-4 sm:mx-4 md:mx-4 md:w-1/2 w-3/4 border-solid border">
+        <div className="self-start rounded-full relative flex items-center justify-center text-white px-4 border-solid border max-w-[150px] max-h-[150px]" 
+          onMouseEnter={handleImageMouseEnter}
+          onMouseLeave={handleImageMouseLeave}
+          onClick={handleImageClick}>
+          {isImageEditable ? (
+            <>
+              <input
+                type="file"
+                accept="image/*"
+                className="absolute inset-0 w-full h-full opacity-0 cursor-pointer z-10"
+                onChange={handleImageUpload}
+              />
+              <img src={image} alt="self" className="w-full h-full object-cover rounded-full" />
+              </>
+            ) : (
+              <>
+              <img src={image} alt="self" className="w-full h-full object-cover rounded-full" />
+              {isImageHovered && (
+                  <div className="absolute top-2 right-2">
+                    <FaPencilAlt className="text-white text-xl" />
+                  </div>
+                )}
+              </>
+            )}
+        </div>
+        <div className="flex flex-col justify-start px-4 md:pl-4 border-solid border w-full">
+          {/* stack of username and bio */}
+          <div className="text-white border-solid border relative p-1 my-4"
+            onMouseEnter={handleUsernameMouseEnter}
+            onMouseLeave={handleUsernameMouseLeave}
+            onClick={handleUsernameClick}
+          >
+            {isUsernameEditable ? (
               <input
                 type="text"
                 defaultValue={username}
@@ -55,7 +98,7 @@ export default function UserProfileHeading() {
               />
             ) : (
               <>
-                {username}
+                <span className="text-xl">{username}</span>
                 {isUsernameHovered && (
                   <div className="absolute top-2 right-2">
                     <FaPencilAlt className="text-white text-xl" />
@@ -63,12 +106,27 @@ export default function UserProfileHeading() {
                 )}
               </>
             )}
-                </div>
-                <div className="text-white border-solid border transition-opacity hover:opacity-50">
-                    {"if the bio is much longer what happens to the size of the box"}
-                </div>
-            </div>
-        </div >
+          </div>
+          <div className="text-white border-solid border relative p-1" onMouseEnter={handleBioMouseEnter} onMouseLeave={handleBioMouseLeave} onClick={handleBioClick}>
+            {isBioEditable ? (
+              <input
+                type="text"
+                defaultValue={bioInfo}
+                onChange={(e) => setBioInfo(e.target.value)}
+              />
+            ) : (
+              <>
+                {bioInfo}
+                {isBioHovered && (
+                  <div className="absolute top-2 right-2">
+                    <FaPencilAlt className="text-white text-xl" />
+                  </div>
+                )}
+              </>
+            )}
+          </div>
+        </div>
+      </div >
     </>
   );
 

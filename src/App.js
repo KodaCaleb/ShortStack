06e6c29ae38bd5f9075f75446e94f0ce4f9b3eca
Jstanion <123262263+Jstanion@users.noreply.github.com>
2023-07-18 18:19:43 -0500

    created MyAccount page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./pages/layout";
 import Profile from "./pages/Profile";
+import MyAccount from "./pages/MyAccount";
 import NotFound from "./pages/NotFound";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route path="/" element={<Home />} />
           <Route path="/profile" element={<Profile />} />
+          <Route path="/account" element={<MyAccount />} />
         </Route>
         <Route path="*" element={<NotFound />} />
       </Routes>  

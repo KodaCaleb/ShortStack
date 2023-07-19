8ad2acf864dbbf7e24c74f50df00e4d9d15939ef
Jstanion <123262263+Jstanion@users.noreply.github.com>
2023-07-18 23:34:37 -0500

    created post container component for modularization

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,27 +1,10 @@
 import React from "react";
-import Video from "../components/VideoContainer/video";
-import { AiOutlineHeart } from 'react-icons/ai';
-import { BiCommentDetail, BiShare, BiBookmarks } from 'react-icons/bi';
-import PostContainer from "../components/VideoContainer/PostContainer";
+import PostContainer from "../components/VideoContainer/PostContainer"
 
 export default function Home() {
   return (
-    <div className="bg h-screen flex pt-40 justify-center">
-        <div className="flex flex-row">
-          <div className="bg-black h-4/5 rounded-2xl p-2 w-auto">
-            <div className="app_videos h-full w-full max-w-500px relative rounded-2xl overflow-scroll">
-              <Video />
-              <Video />
-              <Video />
-            </div>
-          </div>
-          <div className="">
-          <a href=""><AiOutlineHeart className="m-5" style={{color: 'tan'}} size={32} /></a>
-          <a href=""><BiCommentDetail className="m-5" style={{color: 'tan'}} size={32} /></a>
-          <a href=""><BiBookmarks className="m-5" style={{color: 'tan'}} size={32} /></a>
-          <a href=""><BiShare className="m-5" style={{color: 'tan'}} size={32} /></a>
-          </div>
-        </div>
+    <div>
+      <PostContainer />
     </div>
   );
-}
+};

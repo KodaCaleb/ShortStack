4e4ddc777e378aff96a4cd6996e620d0c4b12469
Jstanion <123262263+Jstanion@users.noreply.github.com>
2023-07-19 18:59:48 -0500

    added function to retrieve all users from the database

--- server/users/index.js
+++ server/users/index.js
@@ -4,25 +4,27 @@ const  {collection, getFirestore, getDocs, doc}  = require ("@firebase/firestore
 const firestore = getFirestore(firebase)
 const users = collection( firestore,'Users');
 
+// Get function for all users
+getDocs(users)
+.then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            console.log(doc.id, '=>', doc.data());
+        });
+    })
+    .catch((error) => {
+        console.log("error getting documents: ", error);
+    });
+
 const userDocRef = doc(users, '1AgshjHIigujTKEXtVQR')
 const nestedCollectionRef = collection(userDocRef, 'userInfo')
 
-
+// Get function for a user by ID
 getDocs(nestedCollectionRef)
 .then((querySnapshot) => {
     querySnapshot.forEach((doc) => {
         console.log(doc.id, '=>', doc.data());
-      });
+        });
 })
 .catch((error) => {
     console.log("error getting document: ", error)
 })
-
-
-
-
-// if (!doc.exists) {
-//     console.log('No such document!');
-//   } else {
-//     console.log('Document data:', doc.data());
-//   }

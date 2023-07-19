c5c76753a15a557bbcab998af70d4b2232d271bf
Jstanion <123262263+Jstanion@users.noreply.github.com>
2023-07-18 20:12:43 -0500

    added heart icon to video screen

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,20 @@
 import React from "react";
 import Video from "../components/VideoContainer/video";
+import { FaHeart } from 'react-icons/fa';
 
 export default function Home() {
   return (
     <div className="bg h-screen flex pt-40 justify-center">
-      <div className="video-frame h-800 p-2 w-auto">
-        <div className="app_videos h-full w-full max-w-500px relative rounded-2xl overflow-scroll">
-          <Video />
-          <Video />
-          <Video />
+      <div className="flex flex-row items-end">
+        <div className="video-frame h-800 p-2 w-auto">
+          <div className="app_videos h-full w-full max-w-500px relative rounded-2xl overflow-scroll">
+            <Video />
+            <Video />
+            <Video />
+          </div>
+        </div>
+        <div className="m-5" style={{color: 'red'}}>
+          <FaHeart size={32} />
         </div>
       </div>
     </div>

3ade52995d7887d08be4a9203618adae635df6f0
Jstanion <123262263+Jstanion@users.noreply.github.com>
2023-07-19 21:45:40 -0500

    added function to target specific key values for front end

--- server/users/index.js
+++ server/users/index.js
@@ -1,21 +1,9 @@
 const { firebase } = require("../config/firebase");
 const { collection, getFirestore, getDocs, where,  doc, query } = require("@firebase/firestore");
-
 const firestore = getFirestore(firebase);
 
 // Create a reference to the Users collection
 const users = collection( firestore,'Users');
-
-// Get data for all users
-// getDocs(users)
-//     .then((querySnapshot) => {
-//         querySnapshot.forEach((doc) => {
-//             console.log(doc.id, '=>', doc.data());
-//         });
-//     })
-//     .catch((error) => {
-//         console.log("error getting documents: ", error);
-//     });
 
 // Define the specific user ID
 const userId = '1AgshjHIigujTKEXtVQR';
@@ -29,6 +17,30 @@ const nestedCollectionName = 'userInfo';
 // Create a reference to the nested subcollection using the Users document reference
 const nestedCollectionRef = collection(userDocRef, nestedCollectionName);
 
+// Create a reference to the ID of the targeted document
+const subDocId = 'account';
+
+// Create a reference to the specific element being targeted
+const inputField = 'email'
+
+// Get the data for a single subdocument
+getDocs(nestedCollectionRef)
+.then((querySnapshot) => {
+    // Loops through the data and returns all available subdocuments
+    querySnapshot.forEach((doc) => {
+        // Matches the specific subdocument wanted
+        if (doc.id === subDocId) {
+            // Targets the specific key/value data for front end use
+            console.log(subDocId, '=>', doc.data()[inputField]);
+        };
+    });
+})
+.catch((error) => {
+    console.log("error getting document: ", error);
+})
+
+// extra functions for possible use later..
+
 // Get the data for a single user by ID 
 // getDocs(nestedCollectionRef)
 //     .then((querySnapshot) => {
@@ -39,19 +51,14 @@ const nestedCollectionRef = collection(userDocRef, nestedCollectionName);
 //     .catch((error) => {
 //         console.log("error getting document: ", error);
 //     })
-
-// Create a reference to the ID of the targeted document
-const subDocId = 'profile';
-
-// // Get the data for a specific subcollection document
-getDocs(nestedCollectionRef)
-    .then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-            if (doc.id === subDocId) {
-                console.log(subDocId, '=>', doc.data());
-            };
-        });
-    })
-    .catch((error) => {
-        console.log("error getting document: ", error);
-    })
+        
+// Get data for all users
+// getDocs(users)
+//     .then((querySnapshot) => {
+//         querySnapshot.forEach((doc) => {
+//             console.log(doc.id, '=>', doc.data());
+//         });
+//     })
+//     .catch((error) => {
+//         console.log("error getting documents: ", error);
+//     });

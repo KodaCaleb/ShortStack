64367a97ec7d8a061518554a31864e1e83287f3d
KodaCaleb <122597981+KodaCaleb@users.noreply.github.com>
2023-07-20 16:44:59 -0500

    videos displaying in dom from blob storage

--- src/components/VideoContainer/video.js
+++ src/components/VideoContainer/video.js
@@ -1,17 +1,42 @@
-import { useRef, useState } from "react";
-import sampleVid1 from "../../assets/sampleVid1.mp4";
+// Importing necessary hooks and functions from React and Firebase
+import { useRef, useState, useEffect } from "react";
 import VideoFooter from "./videoFooter";
+import { getDownloadURL, ref } from "firebase/storage";
+import { storage } from "../../firebase";
+
+// Component for displaying a video
+export default function Video({ videoData }) {
+  const [playing, setPlaying] = useState(false); // State for managing whether the video is currently playing
+  const videoRef = useRef(null); // A ref that points to the video element in the DOM
 
 
-export default function Video() {
-  const [playing, setPlaying] = useState(false);
-  const videoRef = useRef(null);
+
+  useEffect(() => {
+     // Extracting the file path from videoData.vidRef, removing the initial part of the Firebase storage URL to keep only the path to the file.
+    const filepath = videoData.vidRef.replace('gs://project3-15aff.appspot.com/', '')
+    // Creating a reference to the video file in Firebase Storage
+    const videoStorageRef = ref(storage, filepath);
+    // Fetching the URL of the video file
+    getDownloadURL(videoStorageRef)
+    .then((url) => {
+      // Assigning the fetched URL to the video element's src attribute to display the video
+      videoRef.current.src = url;
+    })
+    .catch((error) => {
+      // Logging any errors that happen while fetching the video URL
+      console.log("error getting video url", error);
+    })
+    ;
+  }, [videoData]); // This useEffect hook runs whenever videoData changes
+
 
   const onVideoPress = () => {
     if (playing) {
+      // If the video is currently playing, pause the video and update the state
       videoRef.current.pause();
       setPlaying(false);
     } else {
+      // If the video is currently paused, play the video and update the state
       videoRef.current.play();
       setPlaying(true);
     }
@@ -20,11 +45,11 @@ export default function Video() {
   return (
    <div className="relative bg-white w-full h-full videoContainer mt-4">
       <video className="object-fill w-full h-full" 
-      src={sampleVid1} 
-      ref={videoRef}
-      onClick={onVideoPress} 
-      loop 
-      ></video>
+        ref={videoRef}
+        onClick={onVideoPress} 
+        loop 
+      >
+      </video>
       <VideoFooter />
     </div>
   );

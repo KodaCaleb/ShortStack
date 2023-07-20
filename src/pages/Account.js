d1876fdd3217ad2ff7d596446ab494e9e0df5fbb
abigailmgarcia <abigailmg31@gmail.com>
2023-07-20 00:36:28 -0500

    added variables

--- src/pages/Account.js
+++ src/pages/Account.js
@@ -3,7 +3,18 @@ import { FaPencilArt } from 'react-icons/fa';
 import PostContainer from "../components/VideoContainer/PostContainer"
 
 export default function Account() {
-    return(
+    const [isImageHovered, setIsImageHovered] = useState(false);
+    const [isImageEditable, setIsImageEditable] = useState(false);
+    const [isUsernameHovered, setIsUsernameHovered] = useState(false);
+    const [isUsernameEditable, setIsUsernameEditable] = useState(false);
+    const [isBioHovered, setIsBioHovered] = useState(false);
+    const [isBioEditable, setIsBioEditable] = useState(false);
+
+    const[image, setImage] = useState(
+        process.env.PUBLIC_URL + '/pancakeholder.img.png'
+    );
+
+    return (
         <div className="h-screen">
             <p className="text-white">This is my account!</p>
         </div>

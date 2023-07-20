ed3523e9e33e764ec9efa3372cfb180a4e2d054a
Joaquin Salazar <joaqsala@gmail.com>
2023-07-20 14:02:41 -0500

    removed duplicate bgs on PostContainer

--- src/components/VideoContainer/PostContainer.js
+++ src/components/VideoContainer/PostContainer.js
@@ -5,7 +5,7 @@ import { BiCommentDetail, BiShare, BiBookmarks } from 'react-icons/bi';
 
 export default function PostContainer() {
     return (
-        <div className="bg h-screen flex pt-40 justify-center">
+        <div className="h-screen flex pt-40 justify-center">
             <div className="flex flex-row">
                 <div className="bg-black h-4/5 rounded-2xl p-2 w-auto">
                     <div className="app_videos h-full w-full max-w-500px relative rounded-2xl overflow-scroll">

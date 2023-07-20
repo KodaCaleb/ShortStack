0be0a39707a4fb850491e44f4b11131a017e03da
Jstanion <123262263+Jstanion@users.noreply.github.com>
2023-07-19 21:13:58 -0500

    created get function for subcollection documents

--- server/users/index.js
+++ server/users/index.js
@@ -1,26 +1,26 @@
 const { firebase } = require("../config/firebase");
-const { collection, getFirestore, getDocs, doc } = require("@firebase/firestore");
+const { collection, getFirestore, getDocs, where,  doc, query } = require("@firebase/firestore");
 
 const firestore = getFirestore(firebase);
 
 // Create a reference to the Users collection
 const users = collection( firestore,'Users');
 
-// Get function for all users
-getDocs(users)
-    .then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-            console.log(doc.id, '=>', doc.data());
-        });
-    })
-    .catch((error) => {
-        console.log("error getting documents: ", error);
-    });
+// Get data for all users
+// getDocs(users)
+//     .then((querySnapshot) => {
+//         querySnapshot.forEach((doc) => {
+//             console.log(doc.id, '=>', doc.data());
+//         });
+//     })
+//     .catch((error) => {
+//         console.log("error getting documents: ", error);
+//     });
 
-// Create a reference to the specific user ID
+// Define the specific user ID
 const userId = '1AgshjHIigujTKEXtVQR';
 
-// Get a reference to the Users document
+// Create a reference to the Users document
 const userDocRef = doc(users, userId);
 
 // Define the name of the nested subcollection you want to access
@@ -29,21 +29,29 @@ const nestedCollectionName = 'userInfo';
 // Create a reference to the nested subcollection using the Users document reference
 const nestedCollectionRef = collection(userDocRef, nestedCollectionName);
 
-// Get function for retrieving a user and nested info by ID
+// Get the data for a single user by ID 
+// getDocs(nestedCollectionRef)
+//     .then((querySnapshot) => {
+//         querySnapshot.forEach((doc) => {
+//             console.log(doc.id, '=>', doc.data());
+//             });
+//     })
+//     .catch((error) => {
+//         console.log("error getting document: ", error);
+//     })
+
+// Create a reference to the ID of the targeted document
+const subDocId = 'profile';
+
+// // Get the data for a specific subcollection document
 getDocs(nestedCollectionRef)
     .then((querySnapshot) => {
         querySnapshot.forEach((doc) => {
-            console.log(doc.id, '=>', doc.data());
-            });
+            if (doc.id === subDocId) {
+                console.log(subDocId, '=>', doc.data());
+            };
+        });
     })
     .catch((error) => {
-        console.log("error getting document: ", error)
+        console.log("error getting document: ", error);
     })
-
-// Create a reference to the ID of the targeted document
-const subCollectionDocId = 'profile';
-
-// Create a reference to the specific document within the nested subcollection
-const subCollectionDocRef = doc(nestedCollectionRef, subCollectionDocId);
-
-// Get function for a users profile info

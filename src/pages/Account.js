9848712451e3feab9ca1c7cab011f8dc1a96dc0c
abigailmgarcia <abigailmg31@gmail.com>
2023-07-20 00:37:38 -0500

    added hoverable handles and pencil

--- src/pages/Account.js
+++ src/pages/Account.js
@@ -13,7 +13,51 @@ export default function Account() {
     const[image, setImage] = useState(
         process.env.PUBLIC_URL + '/pancakeholder.img.png'
     );
-
+    const [username, setUsername] = useState('Username');
+    const [bioInfo, setBioInfo] = useState('Here for the lulz');
+  
+    const handleImageMouseEnter = () => {
+      setIsImageHovered(true);
+    };
+  
+    const handleImageMouseLeave = () => {
+      setIsImageHovered(false);
+    };
+  
+    const handleImageClick = (e) => {
+      e.preventDefault();
+      setIsImageEditable(!isImageEditable);
+    };
+  
+    const handleImageUpload = (e) => {
+      const file = e.target.files[0];
+      // TODO add code to handle saving image and to use setImage useState
+    };
+  
+    const handleUsernameMouseEnter = () => {
+      setIsUsernameHovered(true);
+    };
+  
+    const handleUsernameMouseLeave = () => {
+      setIsUsernameHovered(false);
+    };
+  
+    const handleUsernameClick = () => {
+      setIsUsernameEditable(!isUsernameEditable);
+    };
+  
+    const handleBioMouseEnter = () => {
+      setIsBioHovered(true);
+    };
+  
+    const handleBioMouseLeave = () => {
+      setIsBioHovered(false);
+    };
+  
+    const handleBioClick = () => {
+      setIsBioEditable(!isBioEditable);
+    };
+  
     return (
         <div className="h-screen">
             <p className="text-white">This is my account!</p>

96f557ee5f05d524d031dd1b70abef60c4f0fb34
cjsolis <cjelizondo12@gmail.com>
2023-07-21 13:08:34 -0500

    added eventhandlers for storing users inputs in signup form

--- src/components/modals/SignUp.js
+++ src/components/modals/SignUp.js
@@ -1,6 +1,35 @@
 import React from "react";
+import { useState } from "react";
 
-export default function SignUpModal({closeModal, toggleModalMode}) {
+export default function SignUpModal({ closeModal, toggleModalMode }) {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // Event handlers for users entering data
+  const handleFirstNameChange = (e) => {
+    setFirstName(e.target.value);
+  };
+  const handleLastNameChange = (e) => {
+    setLastName(e.target.value);
+  };
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleLogData = () => {
+    console.log("First Name:", firstName);
+    console.log("Last Name:", lastName);
+    console.log("Username or Email:", email);
+    console.log("Password:", password);
+  };
+
   return (
     <>
       {/* Modal */}
@@ -14,7 +43,7 @@ export default function SignUpModal({closeModal, toggleModalMode}) {
           <div className="mb-4 md:flex md:justify-between">
             <div class="mb-4 md:mr-2 md:mb-0">
               <label
-                class="block mb-2 text-sm font-bold text-gray-700"
+                class="block mb-2 text-sm font-bold text-yellow-300"
                 for="firstName"
               >
                 First Name
@@ -24,11 +53,13 @@ export default function SignUpModal({closeModal, toggleModalMode}) {
                 id="firstName"
                 type="text"
                 placeholder="First Name"
+                value={firstName}
+                onChange={handleFirstNameChange}
               />
             </div>
             <div className="md:ml-2">
               <label
-                class="block mb-2 text-sm font-bold text-gray-700"
+                class="block mb-2 text-sm font-bold text-yellow-300"
                 for="lastName"
               >
                 Last Name
@@ -38,22 +69,27 @@ export default function SignUpModal({closeModal, toggleModalMode}) {
                 id="lastName"
                 type="text"
                 placeholder="Last Name"
+                value={lastName}
+                onChange={handleLastNameChange}
               />
             </div>
           </div>
           <label
-            className="block mb-2 text-sm font-bold text-gray-700"
-            for="usernameField"
+            className="block mb-2 text-sm font-bold text-yellow-300"
+            for="emailField"
           >
             {" "}
-            Username or Email
+            Email
           </label>
           <input
             className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-            type="text" placeholder="Username or Email"
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={handleEmailChange}
           />
           <label
-            className="block mb-2 text-sm font-bold text-gray-700"
+            className="block mb-2 text-sm font-bold text-yellow-300"
             for="passwordField"
           >
             {" "}
@@ -61,20 +97,33 @@ export default function SignUpModal({closeModal, toggleModalMode}) {
           </label>
           <input
             className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-            type="password" placeholder="password"
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={handlePasswordChange}
           />
-              <div className="flex mt-6 justify-center text-xs">
-            <a href="#" className="text-blue-499 hover:text-yellow-300" onClick={toggleModalMode}>
+          <div className="flex mt-6 justify-center text-xs">
+            <a
+              href="#"
+              className="text-blue-499 hover:text-yellow-300"
+              onClick={toggleModalMode}
+            >
               Back to Login
             </a>
           </div>
           <div className="flex items-center justify-center">
-          <button className="flex items-center justify-center h-12 px-6 w-64 bg-yellow-500 mt-8 rounded font-semibold text-sm text-blue-100 hover:bg-yellow-300">
-            Create
-          </button>
+            <button 
+            className="flex items-center justify-center h-12 px-6 w-64 bg-yellow-500 mt-8 rounded font-semibold text-sm text-blue-100 hover:bg-yellow-300"
+            onClick={handleLogData}
+            >
+              Create
+            </button>
           </div>
-          <button className=" absolute top-2 right-2 px-2 py-2" onClick={closeModal}>
-           X
+          <button
+            className=" absolute top-2 right-2 px-2 py-2"
+            onClick={closeModal}
+          >
+            X
           </button>
         </form>
       </div>

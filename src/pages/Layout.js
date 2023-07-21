8a340c6bb5faa512a9840a68ac420feff3ae8b21
abigailmgarcia <abigailmg31@gmail.com>
2023-07-20 22:50:01 -0500

    fixed nav bar problem

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,10 +1,5 @@
 import { Outlet } from "react-router-dom"
-<<<<<<< HEAD:src/pages/layout.js
-import Navbar from "../components/header/navBar"
-
-=======
 import NavBar from "../components/header/NavBar"
->>>>>>> cfe45b259f37d1899d9e1c405cc8959489066637:src/pages/Layout.js
 
 export default function Layout() {
     return (

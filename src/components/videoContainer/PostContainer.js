2f34056457d55a7c9d5b0ff98c803cf8de9eb8af
csullins <camilesullins@gmail.com>
2023-07-22 18:10:41 -0500

    removing borders

--- src/components/videoContainer/PostContainer.js
+++ src/components/videoContainer/PostContainer.js
@@ -6,12 +6,16 @@ import { BiCommentDetail, BiShare, BiBookmarks } from 'react-icons/bi';
 export default function PostContainer({ videoData }) {
     return (
         <div className="flex justify-center flex-row snap start">
-            <div className=" h-full rounded-2xl p-2 w-3/4 bg-black">
+            <div className=" h-full rounded-3xl p-5 w-3/4 bg-black">
+            {/* How can we import the username variable here? */}
+                <div className="username text-amber-200 text-xl"><p>Username | Bio </p>
+                <p>Video Description</p></div>
+                <hr className="mt-2 mb-2"/>
                 <div className="app_videos h-full flex justify-center relative rounded-2xl overflow-scroll">
                     <Video videoData={videoData} />
                 </div>
             </div>
-            <div className="border-4 border-rose-200">
+            <div>
                 <a href="/"><AiOutlineHeart className="m-5" style={{color: 'tan'}} size={32} /></a>
                 <a href="/"><BiCommentDetail className="m-5" style={{color: 'tan'}} size={32} /></a>
                 <a href="/"><BiBookmarks className="m-5" style={{color: 'tan'}} size={32} /></a>

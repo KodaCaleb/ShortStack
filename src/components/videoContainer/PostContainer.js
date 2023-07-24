26f0bca73b61b9593df303379a620c903f45c3d7
abigailmgarcia <abigailmg31@gmail.com>
2023-07-24 16:34:45 -0500

    logic for follow and unfollow

--- src/components/videoContainer/PostContainer.js
+++ src/components/videoContainer/PostContainer.js
@@ -29,10 +29,10 @@ async function getUserData(userId) {
 export default function PostContainer({ videoData }) {
   const [userData, setUserData] = useState(null);
   const [userHasLiked, setUserHasLiked] = useState(false);
-  // const [isFollowing, setIsFollowing] = useState(false); // variables for follow buttons
+  const [isFollowing, setIsFollowing] = useState(false); // variables for follow buttons
 
   const { user, loading } = useContext(AuthContext); // Destructure user and loading from the context
-  
+
   const uid = user ? user.uid : null; // Get the uid from the user object
 
   useEffect(() => {
@@ -47,7 +47,7 @@ export default function PostContainer({ videoData }) {
         const videoRef = doc(firestore, "videos", videoData.id);
         const likeRef = doc(videoRef, "likes", uid);
         const docSnap = await getDoc(likeRef);
-      
+
         if (docSnap.exists()) {
           setUserHasLiked(true);
         } else {
@@ -63,12 +63,12 @@ export default function PostContainer({ videoData }) {
     if (videoId && userId && !loading) { // ensure neither is undefined or loading
       const videoRef = doc(firestore, "videos", videoId);
       const likeRef = doc(videoRef, "likes", userId);
-    
+
       if ((await getDoc(likeRef)).exists()) {
         console.log("User has already liked this video.");
         return;
       }
-    
+
       await setDoc(likeRef, {});
 
       // Add video reference to the user's liked videos subcollection
@@ -81,11 +81,11 @@ export default function PostContainer({ videoData }) {
         if (!videoDoc.exists()) {
           throw "Document does not exist!";
         }
-    
+
         const newLikesCount = (videoDoc.data().likes || 0) + 1;
         transaction.update(videoRef, { likes: newLikesCount });
       });
-      
+
       setUserHasLiked(true); // set userHasLiked to true after a successful like action
     }
   }
@@ -118,38 +118,51 @@ export default function PostContainer({ videoData }) {
 
   const [showCommentSection, setShowCommentSection] = useState(false);
   async function handleCommentBtnClick() {
-// fetch comments for video 
-// post comments for video
+    // fetch comments for video 
+    // post comments for video
   }
 
-    // Logic for follow button?//
-//   const followUser= async () => {
-//     //Logic for following user
-//     if(!isFollowing) {
-//       try {
-//         const CurrentUserUid = ''; //pull current user id
-//         const UserToFollowUid = videoData.userId; //based off video data containing the uid
+  // Logic for follow button?//
+    const followUser= async () => {
+      //Logic for following user
+      if(!isFollowing) {
+        try {
+          const currentUserUid = ''; //pull current user id
+          const userToFollowUid = videoData.userId; //based off video data containing the uid
 
-//          // Add document in "following" subcollection of the current user.
-//       const followingRef = doc(firestore, 'Users', currentUserUid, 'following', userToFollowUid);
-//       await setDoc(followingRef, {});
+           // Add document in "following" subcollection of the current user.
+        const followingRef = doc(firestore, 'Users', currentUserUid, 'following', userToFollowUid);
+        await setDoc(followingRef, {});
 
-//          // Add document in "followers" subcollection of the user being followed.
-//          const followersRef = doc(firestore, 'Users', userToFollowUid, 'followers', currentUserUid);
-//          await setDoc(followersRef, {});
-//          setIsFollowing(true); // Update the state to indicate that the user is now being followed.
-//         } catch (error) {
-//           console.error('Error following the user:', error);
-//         }
-//       }
-//     };
-//   setIsFollowing(true);
-// };
+           // Add document in "followers" subcollection of the user being followed.
+           const followersRef = doc(firestore, 'Users', userToFollowUid, 'followers', currentUserUid);
+           await setDoc(followersRef, {});
+           setIsFollowing(true); // Update the state to indicate that the user is now being followed.
+          } catch (error) {
+            console.error('Error following the user:', error);
+          }
+        }
+      };
 
-// const unfollowUser = async () => {
-//   //Logic for unfollowing user
-//   setIsFollowing(false);
-// };
+  const unfollowUser = async () => {
+    //Logic for unfollowing user
+    if(isFollowing){
+      try {
+        const currentUserUid = uid; //current users
+        const userToFollowUid = videoDate.userId;
+
+        const followingRef = doc(firestore, 'Users', currentUserUid, 'following', UserToFollowUid);
+        await deleteDoc(followingRef);
+
+        const followersRef = doc(firestore, 'User', userToFollowUid, 'followers', currentUserUid);
+        await deleteDoc(followersRef);
+        setIsFollowing(false); //updates to user that is now being unfollowed
+      } catch (error) {
+        console.error('error unfollowing the user:', error);
+      }
+      }
+    }
+  };
 
 
   return (
@@ -167,29 +180,29 @@ export default function PostContainer({ videoData }) {
         <div className="app_videos h-full flex justify-center relative rounded-2xl overflow-scroll">
           <Video videoData={videoData} />
           <div className="flex flex-col-reverse">
-        {userHasLiked ? (
-          <AiFillHeart
-            className="m-4 hover:cursor-pointer"
-            style={{ color: "tan" }}
-            size={28}
-            onClick={() => {
-              console.log('videoData.id:', videoData.id); // debugging logs
-              console.log('uid:', uid);
-              unlikeVideo(videoData.id, uid); // Call the unlikeVideo function
-            }}
-          />
-        ) : (
-          <AiOutlineHeart
-            className="m-4 hover:cursor-pointer"
-            style={{ color: "tan" }}
-            size={28}
-            onClick={() => {
-              console.log('videoData.id:', videoData.id); // debugging logs
-              console.log('uid:', uid);
-              likeVideo(videoData.id, uid);
-            }}
-          />
-        )}
+            {userHasLiked ? (
+              <AiFillHeart
+                className="m-4 hover:cursor-pointer"
+                style={{ color: "tan" }}
+                size={28}
+                onClick={() => {
+                  console.log('videoData.id:', videoData.id); // debugging logs
+                  console.log('uid:', uid);
+                  unlikeVideo(videoData.id, uid); // Call the unlikeVideo function
+                }}
+              />
+            ) : (
+              <AiOutlineHeart
+                className="m-4 hover:cursor-pointer"
+                style={{ color: "tan" }}
+                size={28}
+                onClick={() => {
+                  console.log('videoData.id:', videoData.id); // debugging logs
+                  console.log('uid:', uid);
+                  likeVideo(videoData.id, uid);
+                }}
+              />
+            )}
             <BiCommentDetail
               className="m-4 hover:cursor-pointer"
               style={{ color: "tan" }}
@@ -202,15 +215,15 @@ export default function PostContainer({ videoData }) {
             <BiShare className="m-4" style={{ color: "tan" }} size={28} />
 
             {/* {isFollowing ? ()} */}
-            <RiUserFollowLine className= "m-4 hover:cursor-pointer" 
-            style={{ color:"tan"}} 
-            size={28}
-            onClick={() => {
-             ;
-              console.log("User clicked the RiUserFollowLine icon"); 
+            <RiUserFollowLine className="m-4 hover:cursor-pointer"
+              style={{ color: "tan" }}
+              size={28}
+              onClick={() => {
+                ;
+                console.log("User clicked the RiUserFollowLine icon");
               }}
-              />  
-              {/* UNFOLLOW BUTTON */}
+            />
+            {/* UNFOLLOW BUTTON */}
             {/* ) : ( 
               <RiUserUnfollowFill
             className="m-4 hover:cursor-pointer"
@@ -220,16 +233,16 @@ export default function PostContainer({ videoData }) {
             }}
               />   */}
           </div>
-         </div>
-         {showCommentSection && (
+        </div>
+        {showCommentSection && (
           <>
-          <CommentSection/>
-          {/* fetch comments related to the video post and map them to display in this  section. */}
-          {/* Add code here to display the list of comments */}
-          {/* Add code here to display the form to submit a comment */}
+            <CommentSection />
+            {/* fetch comments related to the video post and map them to display in this  section. */}
+            {/* Add code here to display the list of comments */}
+            {/* Add code here to display the form to submit a comment */}
           </>
-         )}
+        )}
       </div>
     </div>
   );
- }
+}

f545f004de284067514e8f3bcb690087472ffaf6
Joaquin Salazar <joaqsala@gmail.com>
2023-07-23 19:30:53 -0500

    updated Profile.js to remove editing capabilities

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,30 +1,54 @@
-import React, { useState, useEffect, useContext, useRef } from 'react';
-import AuthContext from "../utils/AuthContext";
-import { FaPencilAlt } from 'react-icons/fa';
-import PostContainer from "../components/videoContainer/PostContainer"
-import { firestore } from "../firebase"
-import { doc, runTransaction } from "firebase/firestore"
-import { useDocumentOnce } from 'react-firebase-hooks/firestore';
+import React, { useState, useEffect, useContext } from 'react';
+import AuthContext from '../utils/AuthContext';
+import PostContainer from '../components/videoContainer/PostContainer';
+import { firestore } from '../firebase';
+import { doc, getDoc } from 'firebase/firestore';
 import AccountModal from './Account';
 // import AccountModal from './Account';
 
 export default function UserProfileHeading() {
-  const [isPhotoHovered, setIsPhotoHovered] = useState(false);
-  const [isPhotoEditable, setIsPhotoEditable] = useState(false);
-  const [isUsernameHovered, setIsUsernameHovered] = useState(false);
-  const [isUsernameEditable, setIsUsernameEditable] = useState(false);
-  const [isBioHovered, setIsBioHovered] = useState(false);
-  const [isBioEditable, setIsBioEditable] = useState(false);
   const [isModalOpen, setModalOpen] = useState(false);
   const [isBlurBackground, setBlurBackground] = useState(false);
-  const [editedUsername, setEditedUsername] = useState("");
-  const usernameInputRef = useRef(null); 
+
+  const { user } = useContext(AuthContext); // This is the global user id reference
+
+  // intialize state variables for user
+  const [username, setUsername] = useState("");
+  const [bio, setBio] = useState("");
+  const [photo, setPhoto] = useState("");
+  const [loadingUser, setLoadingUser] = useState(true)
+
+  useEffect(() => {
+    // Wait for the user context to be available
+    if (user) {
+    //  const { uid, displayName, email, photoURL, emailVerified } = user;
+    const getUserData = async () => {
+      try {
+        const userDocRef = doc(firestore, "Users", user.uid)
+        const userDocSnapshot = await getDoc(userDocRef)
+        if (userDocSnapshot.exists()){
+          const userData = userDocSnapshot.data()
+
+          setBio(userData.bio || "")
+        }
+      } catch (error){
+        console.log("Error fetching data from firestore:", error)
+      }
+    };
+
+    getUserData()
+
+      setUsername(user.displayName || "")
+      setPhoto(user.photoURL || process.env.PUBLIC_URL + '/pancakeholder.img.png')
+      setLoadingUser(false);
+    }
+    return () => {
+      // Cleanup code (if needed)
+    };
   
-  const { isLoggedIn, user } = useContext(AuthContext); // This is the global user id reference
-  const { uid, displayName, email, photoURL, emailVerified } = user;
-  
+  }, [user]);
+
   const openModal = () => {
-    console.log("modal should open");
     setModalOpen(true);
     setBlurBackground(true);
   };
@@ -34,247 +58,56 @@ export default function UserProfileHeading() {
     setBlurBackground(false);
   };
 
-  const [photo, setPhoto] = useState(
-    process.env.PUBLIC_URL + '/pancakeholder.img.png'
-  );
-  const [username, setUsername] = useState("");
-  const [bioInfo, setBioInfo] = useState('Here for the lulz');
-
-// starting framework to get fields from profile document
-  const userProfileRef = doc(firestore, 'Users', 'hRStfIZHDNQQC9iaXH8Z', 'userInfo', 'profile');
-  // use the UseDocumentOnce hook 
-  const [profile, loading, error] = useDocumentOnce(userProfileRef);
-
-  const [profileData, setProfileData] = useState(null);
-  // using useEffect to make API call only when profile is updated
-  useEffect(() => {
-    if (!loading && !error && profile && profile.exists()) {
-      setProfileData(profile.data());
-    }
-  }, [loading, error, profile]);
-
-// display when data is being retrieved
-  if (loading) {
-    return <p>Loading...</p>;
-  }
-// display when data is being retrieved
-  if (error) {
-    return <p>Error: {error.message}</p>;
-  }
-
-  if (profileData) { 
-    // Use the data from the "profile" document.
-    const { bio, darkMode, photo, username } = profileData;
-    console.log("Bio:", bio, "Dark Mode:", {darkMode}, "Photo:", {photo}, "Username:", {username})
-  }
-
-
-  const handlePhotoMouseEnter = () => {
-    setIsPhotoHovered(true);
-  };
-
-  const handlePhotoMouseLeave = () => {
-    setIsPhotoHovered(false);
-  };
-
-  const handlePhotoClick = (e) => {
-    e.preventDefault();
-    setIsPhotoEditable(!isPhotoEditable);
-  };
-
-  const handlePhotoUpload = (e) => {
-    const file = e.target.files[0];
-    // TODO add code to handle saving photo and to use setPhoto useState
-  };
-
-  const handleUsernameMouseEnter = () => {
-    setIsUsernameHovered(true);
-  };
-
-  const handleUsernameMouseLeave = () => {
-    setIsUsernameHovered(false);
-  };
-
-  const handleUsernameBlur = async () => {
-    setIsUsernameEditable(false);
-  
-    try {
-      // Update the username in the profileData state
-      setProfileData((prevProfileData) => ({
-        ...prevProfileData,
-        username: editedUsername,
-      }));
-  
-      // TODO: Call the function to update the username in the database with a transaction
-      const userProfileRef = doc(
-        firestore,
-        "Users",
-        "hRStfIZHDNQQC9iaXH8Z",
-        "userInfo",
-        "profile"
-      );
-  
-      await runTransaction(firestore, async (transaction) => {
-        const profileSnapshot = await transaction.get(userProfileRef);
-        if (!profileSnapshot.exists()) {
-          throw new Error("Profile document does not exist!");
-        }
-  
-        // Update the username field in the document
-        transaction.update(userProfileRef, { username: editedUsername });
-      });
-  
-      console.log("Username updated successfully!");
-    } catch (error) {
-      console.error("Error updating username:", error);
-      // Handle the error (e.g., show an error message to the user)
-    }
-  };
-  
-
-  const handleUsernameClick = () => {
-    setIsUsernameEditable(true);
-    setEditedUsername(profileData?.username || '');
-  };
-  
-  const handleBioMouseEnter = () => {
-    setIsBioHovered(true);
-  };
-
-  const handleBioMouseLeave = () => {
-    setIsBioHovered(false);
-  };
-
-  const handleBioClick = () => {
-    setIsBioEditable(!isBioEditable);
-  };
-
-  return ( 
+  return (
     <>
-    <div className={`main-container${isBlurBackground ? ' blur-background' : ''}`}>
-      <div className="flex h-100 flex-col items-center">
-        <div className="flex justify-center md:flex-row mx-4 md:w-1/2 m-20">
-          <div
-            className="self-start rounded-full relative flex items-center justify-center px-4 max-w-[150px] max-h-[150px] border border-white bg-yellow-400"
-            onMouseEnter={handlePhotoMouseEnter}
-            onMouseLeave={handlePhotoMouseLeave}
-            onClick={handlePhotoClick}
-          >
-            {isPhotoEditable ? (
-              <>
-                <input
-                  type="file"
-                  accept="photo/*"
-                  className="absolute inset-0 w-full h-full opacity-0 cursor-pointer z-10"
-                  onChange={handlePhotoUpload}
-                />
-                <img
-                  src={profileData?.photo || process.env.PUBLIC_URL + '/pancakeholder.img.png'}
-                  alt="self"
-                  className="w-full h-full object-cover rounded-full"
-                />
-              </>
-            ) : (
-              <>
-                <img
-                  src={profileData?.photo || process.env.PUBLIC_URL + '/pancakeholder.img.png'}
-                  alt="self"
-                  className="w-full h-full object-cover rounded-full"
-                />
-                {isPhotoHovered && (
-                  <div className="absolute top-2 right-2">
-                    <FaPencilAlt className="text-xl text-white" />
-                  </div>
-                )}
-              </>
-            )}
-          </div>
-          <div className="flex flex-col justify-start px-4 md:pl-4 w-full">
-            {/* stack of username and bio */}
-            <div
-              className="relative p-1 my-4 border border-white text-white"
-              onMouseEnter={handleUsernameMouseEnter}
-              onMouseLeave={handleUsernameMouseLeave}
-              onClick={handleUsernameClick}
-            >
-              {isUsernameEditable ? (
-                <input
-                  type="text"
-                  ref={usernameInputRef}
-                  value={editedUsername}
-                  onChange={(e) => setEditedUsername(e.target.value)} // Update editedUsername, not setUsername
-                  onBlur={handleUsernameBlur} // Save changes when input loses focus
-                  style={{ color: "black" }}
-                />
-              ) : (
-                <>
-                  <span className="text-xl">{profileData?.username}</span>
-                  {isUsernameHovered && (
-                    <div className="absolute top-2 right-2 z-10">
-                      <FaPencilAlt className="text-xl text-white z-10" />
-                    </div>
-                  )}
-                </>
-              )}
+      <div
+        className={`main-container${
+          isBlurBackground ? ' blur-background' : ''
+        }`}
+      >
+        <div className="flex h-100 flex-col items-center">
+          <div className="flex justify-center md:flex-row mx-4 md:w-1/2 m-20">
+            <div className="self-start rounded-full relative flex items-center justify-center px-4 max-w-[150px] max-h-[150px] border border-white bg-yellow-400">
+              <img
+                src={photo}
+                alt="Profile"
+                onError={() => setPhoto(process.env.PUBLIC_URL + '/pancakeholder.img.png')}
+                className="w-full h-full object-cover rounded-full"
+              />
             </div>
-            <div
-              className="relative p-1 border border-white text-white"
-              onMouseEnter={handleBioMouseEnter}
-              onMouseLeave={handleBioMouseLeave}
-              onClick={handleBioClick}
-            >
-              {isBioEditable ? (
-                <input
-                  type="text"
-                  defaultValue={profileData?.bio}
-                  onChange={(e) => setBioInfo(e.target.value)}
-                  style={{ color: "black" }}
-                />
-              ) : (
-                <>
-                  {profileData?.bio}
-                  {isBioHovered && (
-                    <div className="absolute top-2 right-2">
-                      <FaPencilAlt className="text-xl text-white" />
-                    </div>
-                  )}
-                </>
-              )}
-              
+            <div className="flex flex-col justify-start px-4 md:pl-4 w-full">
+              {/* stack of username and bio */}
+              <div className="relative p-1 my-4 border border-white text-white">
+                <span className="text-xl">{username}</span>
+              </div>
+              <div className="relative p-1 border border-white text-white">
+              <span className="text-xl">{bio}</span>
+              </div>
+              <button
+                type="button"
+                className="focus:outline-none text-black bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900 my-4"
+                data-modal-target="authentication-modal"
+                onClick={openModal}
+              >
+                Account Info
+              </button>
+              <AccountModal isOpen={isModalOpen} closeModal={closeModal} />
             </div>
-            <button
-          type="button"
-          className="focus:outline-none text-black bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900 my-4"
-          onClick="something"
-    
-        >
-          Update Profile Information
-        </button>
-            <button
-          type="button"
-          className="focus:outline-none text-black bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900 my-4"
-          data-modal-target="authentication-modal"
-          onClick={openModal}
-        >
-          Account Info
-        </button>
-        <AccountModal isOpen={isModalOpen} closeModal={closeModal} />
           </div>
-        </div>
 
-        {/* </div> */}
-        {/* <AccountModal isOpen={isModalOpen} closeModal={closeModal} /> */}
+          {/* </div> */}
+          {/* <AccountModal isOpen={isModalOpen} closeModal={closeModal} /> */}
 
-        <div className="w-3/4 grid grid-cols-3">
-          {/* <PostContainer />
+          <div className="w-3/4 grid grid-cols-3">
+            {/* <PostContainer />
           <PostContainer />
           <PostContainer />
           <PostContainer />
           <PostContainer />
           <PostContainer /> */}
+          </div>
         </div>
-      </div>
       </div>
     </>
   );
-};
+}

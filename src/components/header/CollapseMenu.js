743912f0572baca246887362c31f07f4a15a17f2
csullins <camilesullins@gmail.com>
2023-07-23 23:45:54 -0500

    search centering and nav response center

--- src/components/header/CollapseMenu.js
+++ src/components/header/CollapseMenu.js
@@ -1,23 +1,22 @@
 import PancakeLogo from "../../assets/pancakeLogo.png";
 import { HandleLogout } from "../../utils/LoginLogout";
-import { useState, useContext } from "react";
+import { useContext } from "react";
 import AuthContext from "../../utils/AuthContext"; // Import the AuthContext
 
 export default function CollapseMenu() {
   const { isLoggedIn } = useContext(AuthContext);
-  const [isMenuOpen, setMenuOpen] = useState(false);
-  const toggleMenu = () => {
-    setMenuOpen(!isMenuOpen);
-    console.log(isMenuOpen);
-  };
 
   return (
     <>
       <div>
         <ul className="main p-0 relative list-none inline-block ">
-          <li className="relative inline-block  mr-2 cursor-pointer z-5 w-20">
-            <img className="h-14 w-11/12 cursor-pointer " src={PancakeLogo} alt="" />
-            <ul className="drop top-12 list-none absolute left-0 z-10">
+          <li className="relative inline-block  mr-4 cursor-pointer z-5 w-20">
+            <img
+              className="h-14 w-11/12 cursor-pointer "
+              src={PancakeLogo}
+              alt=""
+            />
+            <ul className="drop top-12 list-none absolute  z-10">
               <div className="relative p-2 ">
                 <li className="text-white text-center block p-0 align-middle rounded-lg mt-3">
                   <a href="/">Following</a>
@@ -39,7 +38,7 @@ export default function CollapseMenu() {
                 </li>
                 <li className="text-white text-center block p-0 w-full align-middle rounded-lg mt-3">
                   {isLoggedIn ? (
-                    <a onClick={HandleLogout} >Logout</a>
+                    <a onClick={HandleLogout}>Logout</a>
                   ) : (
                     <a href="/login">Login</a>
                   )}

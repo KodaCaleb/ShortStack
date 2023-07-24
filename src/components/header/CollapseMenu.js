e77eeffd3149da098a5f334aeec0c4598775c4b8
Jstanion <123262263+Jstanion@users.noreply.github.com>
2023-07-24 00:20:43 -0500

    adjusted collapsed menu

--- src/components/header/CollapseMenu.js
+++ src/components/header/CollapseMenu.js
@@ -13,22 +13,20 @@ export default function CollapseMenu() {
   };
 
   return (
-    <>
-      <div>
-        <ul className="main p-0 relative list-none inline-block"
-          onMouseEnter={() => toggleMenu(true)} // When the mouse enters, set isMenuOpen to true
-          onMouseLeave={() => toggleMenu(false)} // When the mouse leaves, set isMenuOpen to false
+    <div>
+      <ul className="main p-0 relative list-none inline-block">
+        <li className="relative inline-block  mr-2 cursor-pointer z-5 w-20"
+        onMouseEnter={() => toggleMenu(true)}
+        onMouseLeave={() => toggleMenu(false)}
         >
-          <li className="relative inline-block  mr-2 cursor-pointer z-5 w-20">
-            {isMenuOpen ? (
-              <img className="h-14 w-11/12 cursor-pointer " src={clickedLogo} alt="stack of pancakes with syrup" />
-            ) : (
-              <img className="h-14 w-11/12 cursor-pointer " src={unclickedLogo} alt="stack of pancakes" />
-            )}
-
-            {isLoggedIn ? (
-              <ul className="drop top-12 list-none absolute left-0 z-10">
-                <div className="relative p-2 ">
+          <img className="h-14 w-11/12 cursor-pointer "
+            src={isMenuOpen ? clickedLogo : unclickedLogo} 
+            alt={isMenuOpen ? "stack of pancakes with syrup" : "stack of pancakes"}
+            />
+          <ul className="drop top-12 list-none absolute left-0 z-10">
+            <div className="relative p-2 ">
+              {isLoggedIn ? (
+                <>
                   <li className="text-white text-center block p-0 align-middle rounded-lg mt-3">
                     <a href="/">Following</a>
                   </li>
@@ -50,23 +48,21 @@ export default function CollapseMenu() {
                   <li className="text-white text-center block p-0 w-full align-middle rounded-lg mt-3">
                     <a onClick={HandleLogout} >Logout</a>
                   </li>
-                </div>
-              </ul>
-            ) : (
-              <ul className="drop top-12 list-none absolute left-0 z-10">
-                <div className="relative p-2 ">
+                </>
+              ) : (
+                <>
                   <li className="text-white  block p-0 w-full align-middle text-center rounded-lg mt-3">
                     <a href="/">Explore</a>
                   </li>
                   <li className="text-white text-center block p-0 w-full align-middle rounded-lg mt-3">
                     <a href="/login">Login</a>
                   </li>
-                </div>
-              </ul>
-            )}
-          </li>
-        </ul>
-      </div>
-    </>
+                </>
+              )}
+            </div>
+          </ul>
+        </li>
+      </ul>
+    </div >
   );
 }

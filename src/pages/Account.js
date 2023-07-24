b577b7cbfa3e0cfa76453f3ba6a778ef226c3733
abigailmgarcia <abigailmg31@gmail.com>
2023-07-23 19:53:26 -0500

    added h3 into form

--- src/pages/Account.js
+++ src/pages/Account.js
@@ -8,12 +8,14 @@ export default function AccountModal({ isOpen, closeModal, toggleModalMode }) {
     return (
         <>
         
-         <div className="flex flex-col items-center justify-center text-yellow-500 ">
-        <h3 className="pt-4 text-2xl text-center"> View Account Info</h3>
+         {/* <div className="flex flex-col items-center justify-center text-yellow-500 ">
+        <h3 className="pt-4 text-2xl text-center"> View Account Info</h3> */}
         <form
           className="relative flex flex-col  bg-black text-white rounded shadow-lg p-12 mt-12 border border-white"
           action=""
         >
+          <div className="flex flex-col items-center justify-center text-yellow-500 ">
+        <h3 className="pt-4 text-2xl text-center"> View Account Info</h3>
           <div className="mb-4 md:flex md:justify-between">
             <div className="mb-4 md:mr-2 md:mb-0">
               <label
@@ -90,8 +92,8 @@ export default function AccountModal({ isOpen, closeModal, toggleModalMode }) {
           <button className=" absolute top-2 right-2 px-2 py-2" onClick={closeModal}>
            X
           </button>
+          </div>
         </form>
-      </div>
     </>
     );
 };

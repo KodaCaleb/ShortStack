acb3ae12f7e66d16243b2d14ad544d5ca7c1ec0c
KodaCaleb <122597981+KodaCaleb@users.noreply.github.com>
2023-07-23 23:12:35 -0500

    logic for mapping tags to tagInput array

--- src/components/uploadLogic/TagsInput.js
+++ src/components/uploadLogic/TagsInput.js
@@ -1,7 +1,29 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
-export default function TagsInput({ value=[], onChange }) {
-    const [tag, setTag] = useState('');
+export default function TagsInput({ value = [], onChange }) {
+  const [tag, setTag] = useState("");
 
-    const removeTag = (indexToRemove) => {
-        onChange
+  const removeTag = (indexToRemove) => {
+    onChange(value.filter((_, index) => index !== indexToRemove));
+  };
+
+  const addTag = (event) => {
+    event.preventDefault();
+    onChange([...value, tag]);
+    setTag("");
+  };
+
+  return (
+    <div>
+      {value.map((tag, index) => (
+        <button key={tag} onClick={() => removeTag(index)}>
+          {tag} x
+        </button>
+      ))}
+      <form onSubmit={addTag}>
+        <input value={tag} onChange={(e) => setTag(e.target.value)} />
+        <button type="submit">Add tag</button>
+      </form>
+    </div>
+  );
+}

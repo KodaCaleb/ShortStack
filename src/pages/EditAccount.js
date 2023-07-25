71d79a2c25913b8722d65b700c0cd7f9cf544c96
cjsolis <cjelizondo12@gmail.com>
2023-07-25 10:29:08 -0500

    account container and inputs sized for mobile

--- src/pages/EditAccount.js
+++ src/pages/EditAccount.js
@@ -5,7 +5,7 @@ import { updateEmail } from "firebase/auth";
 import AuthContext from "../utils/AuthContext";
 import { FaPencilAlt } from "react-icons/fa";
 import { getAuth, deleteUser } from "firebase/auth";
-import { useDeleteAccount } from "../utils/UserDeleteAccount"
+import { useDeleteAccount } from "../utils/UserDeleteAccount";
 import { useNavigate } from "react-router-dom";
 
 export default function EditAccount() {
@@ -69,117 +69,125 @@ export default function EditAccount() {
   const handleDeleteAccount = useDeleteAccount();
 
   return (
-    <> <div className="flex items-center justify-center h-screen">
-      <form
-        className="flex flex-col  bg-black text-white rounded shadow-lg p-12 mt-12 border border-white"
-        action=""
-      >
-        <div className="flex flex-col items-center justify-center text-yellow-500 ">
-          <h3 className="p-2 my-3 text-2xl text-center"> Account Information</h3>
+    <>
+      {" "}
+      <div className="flex items-center justify-center h-screen">
+        <form
+          className="flex flex-col  bg-black text-white rounded shadow-lg p-12 mt-12 border border-white"
+          action=""
+        >
+          <div className="flex flex-col  text-yellow-500 ">
+            <h3 className="p-2 my-3 text-2xl text-center">
+              {" "}
+              Account Information
+            </h3>
 
-          {/* FirstName and LastName input boxes */}
-          <div className="mb-4 md:flex md:justify-between">
-            <div className="mb-4 md:mr-2 md:mb-0">
-              <label className="block mb-2 text-sm font-bold text-gray-700">
-                First Name
+            {/* FirstName and LastName input boxes */}
+            <div className="mb-4 md:flex md:justify-between">
+              <div className="mb-4 md:mr-2 md:mb-0">
+                <label className="block mb-2 text-sm font-bold text-gray-700">
+                  First Name
+                </label>
+                <input
+                  className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                  id="firstName"
+                  type="text"
+                  placeholder="First Name"
+                  value={updatedFirstName || ""}
+                  onChange={(e) => setUpdatedFirstName(e.target.value)}
+                />
+              </div>
+              <div className="md:ml-2">
+                <label
+                  className="block mb-2 text-sm font-bold text-gray-700"
+                  htmlFor="lastName"
+                >
+                  Last Name
+                </label>
+                <input
+                  className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                  id="lastName"
+                  type="text"
+                  placeholder="Last Name"
+                  value={updatedLastName || ""}
+                  onChange={(e) => setUpdatedLastName(e.target.value)}
+                />
+              </div>
+            </div>
+
+            {/* Email, password, devRole */}
+            <div className=" flex flex-col items-start">
+              <label
+                className="mb-2 text-sm font-bold text-gray-700"
+                htmlFor="usernameField"
+              >
+                {" "}
+                Email
               </label>
               <input
-                className="flex-grow px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-                id="firstName"
+                className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
                 type="text"
-                placeholder="First Name"
-                value={updatedFirstName || ""}
-                onChange={(e) => setUpdatedFirstName(e.target.value)}
+                placeholder="Email"
+                value={updatedEmail || user?.email || ""}
+                onChange={(e) => setUpdatedEmail(e.target.value)}
               />
-            </div>
-            <div className="md:ml-2">
+              <label
+                className="mb-2 text-sm font-bold text-gray-700"
+                htmlFor="passwordField"
+              >
+                {" "}
+                Password
+              </label>
+              <input
+                className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                type="password"
+                placeholder="password"
+              />
               <label
                 className="block mb-2 text-sm font-bold text-gray-700"
-                htmlFor="lastName"
+                htmlFor="phoneNumberField"
               >
-                Last Name
+                {" "}
+                Bio
               </label>
               <input
-                className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-                id="lastName"
+                className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
                 type="text"
-                placeholder="Last Name"
-                value={updatedLastName || ""}
-                onChange={(e) => setUpdatedLastName(e.target.value)}
+                placeholder="bio"
+                value={updatedBio || ""}
+                onChange={(e) => setUpdatedBio(e.target.value)}
               />
             </div>
-          </div>
-          
-          {/* Email, password, devRole */}
-    
-          <label
-            className="mb-2 text-sm font-bold text-gray-700"
-            htmlFor="usernameField"
-          >
-            {" "}
-            Email
-          </label>
-          <input
-            className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-            type="text"
-            placeholder="Email"
-            value={updatedEmail || user?.email || ""}
-            onChange={(e) => setUpdatedEmail(e.target.value)}
-          />
-          <label
-            className="block mb-2 text-sm font-bold text-gray-700"
-            htmlFor="passwordField"
-          >
-            {" "}
-            Password
-          </label>
-          <input
-            className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-            type="password"
-            placeholder="password"
-          />
-          <label
-            className="block mb-2 text-sm font-bold text-gray-700"
-            htmlFor="phoneNumberField"
-          >
-            {" "}
-            Bio
-          </label>
-          <input
-            className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-            type="text"
-            placeholder="bio"
-            value={updatedBio || ""}
-            onChange={(e) => setUpdatedBio(e.target.value)}
-          />
-      
 
-          {/* SaveChanges and Delete Account Buttons */}
-          <div className="flex items-center justify-center">
-            <button
-              className="flex items-center justify-center h-12 px-6 w-64 bg-yellow-500 mt-8 rounded font-semibold text-sm text-blue-100 hover:bg-yellow-300
+            {/* SaveChanges and Delete Account Buttons */}
+            <div className="flex items-center justify-center">
+              <button
+                className="flex items-center justify-center h-12 px-6 w-64 bg-yellow-500 mt-8 rounded font-semibold text-sm text-blue-100 hover:bg-yellow-300
            hover:rounded-3xl
            hover:border-2
            hover:border-amber-700
            hover:w-80 ease-in-out duration-300"
-              onClick={handleEditAccount}
-            >
-              Save Changes
-            </button>
-          </div>
-          {isUpdateSuccess && (
-            <div className="flex items-center justify-center mt-4 text-green-500">
-              Account updated successfully!
+                onClick={handleEditAccount}
+              >
+                Save Changes
+              </button>
             </div>
-          )}
-          <div className="flex mt-6 justify-center text-xs">
-            <a href="#" className="text-blue-499 hover:text-yellow-300"
-            onClick={handleDeleteAccount}>
-              Delete Account
-            </a>
+            {isUpdateSuccess && (
+              <div className="flex items-center justify-center mt-4 text-green-500">
+                Account updated successfully!
+              </div>
+            )}
+            <div className="flex mt-6 justify-center text-xs">
+              <a
+                href="#"
+                className="text-blue-499 hover:text-yellow-300"
+                onClick={handleDeleteAccount}
+              >
+                Delete Account
+              </a>
+            </div>
           </div>
-        </div>
-      </form>
+        </form>
       </div>
     </>
   );

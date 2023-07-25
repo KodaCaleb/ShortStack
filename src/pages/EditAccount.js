0c411b1b116e096bdce674e61d198d50680dc605
cjsolis <cjelizondo12@gmail.com>
2023-07-25 10:32:24 -0500

    changed bio label and placeholder to dev role

--- src/pages/EditAccount.js
+++ src/pages/EditAccount.js
@@ -148,12 +148,12 @@ export default function EditAccount() {
                 htmlFor="phoneNumberField"
               >
                 {" "}
-                Bio
+                Dev Role
               </label>
               <input
                 className="w-full px-3 py-2 mb-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
                 type="text"
-                placeholder="bio"
+                placeholder="Dev Role"
                 value={updatedBio || ""}
                 onChange={(e) => setUpdatedBio(e.target.value)}
               />

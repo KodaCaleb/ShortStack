a8fa27bab95c43a7437720d9635eba27140cc906
cjsolis <cjelizondo12@gmail.com>
2023-07-25 09:46:27 -0500

    function for fetching videos gets back array of videos by searchtag

--- src/components/header/NavBar.js
+++ src/components/header/NavBar.js
@@ -3,11 +3,13 @@ import LoginLogout from "../../utils/LoginLogout";
 import LoginModal from "../modals/Login";
 import CollapseMenu from "./CollapseMenu";
 import AuthContext from "../../utils/AuthContext"; // Import the AuthContext
+import SearchVideosByTags from "../../utils/SearchBarTags";
 
 function Navbar() {
   const { isLoggedIn } = useContext(AuthContext);
   const [isModalOpen, setIsModalOpen] = useState(false);
 
+  
   const openModal = () => {
     setIsModalOpen(true);
   };
@@ -15,6 +17,33 @@ function Navbar() {
   const closeModal = () => {
     setIsModalOpen(false);
   };
+
+  //Search bar state
+  const[searchTag, setSearchTag] = useState("");
+  const [matchingVideos, setMatchingVideos] = useState([])
+
+
+
+
+  // handle Search bar function
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    try{
+      if(searchTag.trim() !== ""){
+        const videos = await SearchVideosByTags(searchTag);
+        console.log("Matching Videos:", videos);
+        setMatchingVideos(videos);
+        console.log(videos)
+      }else{
+        setMatchingVideos([]);
+      }
+   
+    }catch(error){
+      console.error("error searching videos", error)
+    }
+}
+
+
 
   return (
     <header className="w-full">
@@ -91,9 +120,12 @@ function Navbar() {
           <input
             className="bg-yellow-600"
             type="text"
+            value={searchTag}
+            onChange={(e) => setSearchTag(e.target.value)}
+            onSubmit={handleSearch}
             placeholder="Search Tutorials"
           />
-          <div className="search h-4 w-6 bg-yellow-400"></div>
+          <div className="search h-4 w-6 bg-yellow-400" onClick={handleSearch}></div>
         </div>
         <div className="absolute top-5 right-6 mr-1">
           {isLoggedIn ? (
--- src/utils/SearchBarTags.js
+++ src/utils/SearchBarTags.js
@@ -2,48 +2,22 @@ import React, { useEffect, useState } from "react";
 import { collection, query, getDocs, doc, where } from "firebase/firestore";
 import { firestore } from "../firebase";
 
+export default async function SearchVideosByTags(searchTag) {
+  try {
+    const videosCollection = collection(firestore, "videos");
 
-export default function SearchVideosByTags() {
+    const searchedVideos = query(
+      videosCollection,
+      where("tags", "array-contains", searchTag)
+    );
+    const querySnapshot = await getDocs(searchedVideos);
 
-    const [searchTag, setSearchTag] = useState("")
-    const [matchingVideos, setMatchingVideos ] = useState([])
-    const searchVideos = async (searchTag) => {
-        try {
-            const videosCollection = collection(firestore, "videos");
-            const searchedVideos = query(videosCollection, where("", "", searchTag));
-            const querySnapshot = await getDocs(searchedVideos);
+    // Process the query result
+   const matchingVideos = querySnapshot.docs.map((doc) => doc.data())
+        return matchingVideos
 
-            //Process the query result
-
-            const matchingVideos = [];
-            querySnapshot.forEach((doc) => {
-                const videoData = doc.data();
-
-                matchingVideos.push(videoData)
-            });
-            return matchingVideos
-        } catch (error) {
-            console.error("error searching videos", error)
-            return []
-        }
-    }
-
-    const handleSearch = (e) => {
-        e.preventDefault();
-        searchVideos(searchTag)
-    }
-return(
-    // For testing searching, switch with navbar search input
-    <>
-    <input
-    value={searchTag}
-    onChange={(e) => setSearchTag(e.target.value)}
-    />
- <button onClick={handleSearch}></button>
-
-{/* map matching.videos */}
- 
- </>
-)
+  } catch (error) {
+    console.error("Error searching videos", error);
+    return [];
+  }
 }
-

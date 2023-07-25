4e5d8c554a9b3b5e080440fdccadc649324bee26
csullins <camilesullins@gmail.com>
2023-07-24 22:49:23 -0500

    move buttons to the left of vid

--- src/components/videoContainer/PostContainer.js
+++ src/components/videoContainer/PostContainer.js
@@ -30,7 +30,7 @@ export default function PostContainer({ videoData }) {
   const [userHasLiked, setUserHasLiked] = useState(false);
 
   const { user, loading } = useContext(AuthContext); // Destructure user and loading from the context
-  
+
   const uid = user ? user.uid : null; // Get the uid from the user object
 
   useEffect(() => {
@@ -41,11 +41,12 @@ export default function PostContainer({ videoData }) {
 
   useEffect(() => {
     const checkIfLiked = async () => {
-      if (videoData.id && uid && !loading) { // ensure neither is undefined or loading
+      if (videoData.id && uid && !loading) {
+        // ensure neither is undefined or loading
         const videoRef = doc(firestore, "videos", videoData.id);
         const likeRef = doc(videoRef, "likes", uid);
         const docSnap = await getDoc(likeRef);
-      
+
         if (docSnap.exists()) {
           setUserHasLiked(true);
         } else {
@@ -58,15 +59,16 @@ export default function PostContainer({ videoData }) {
   }, [videoData, uid, loading]);
 
   async function likeVideo(videoId, userId) {
-    if (videoId && userId && !loading) { // ensure neither is undefined or loading
+    if (videoId && userId && !loading) {
+      // ensure neither is undefined or loading
       const videoRef = doc(firestore, "videos", videoId);
       const likeRef = doc(videoRef, "likes", userId);
-    
+
       if ((await getDoc(likeRef)).exists()) {
         console.log("User has already liked this video.");
         return;
       }
-    
+
       await setDoc(likeRef, {});
 
       // Add video reference to the user's liked videos subcollection
@@ -79,17 +81,18 @@ export default function PostContainer({ videoData }) {
         if (!videoDoc.exists()) {
           throw "Document does not exist!";
         }
-    
+
         const newLikesCount = (videoDoc.data().likes || 0) + 1;
         transaction.update(videoRef, { likes: newLikesCount });
       });
-      
+
       setUserHasLiked(true); // set userHasLiked to true after a successful like action
     }
   }
 
   async function unlikeVideo(videoId, userId) {
-    if (videoId && userId && !loading) { // ensure neither is undefined or loading
+    if (videoId && userId && !loading) {
+      // ensure neither is undefined or loading
       const videoRef = doc(firestore, "videos", videoId);
       const likeRef = doc(videoRef, "likes", userId);
 
@@ -115,7 +118,7 @@ export default function PostContainer({ videoData }) {
   }
 
   const [showCommentSection, setShowCommentSection] = useState(false);
-  
+
   const handleCloseCommentSection = () => {
     setShowCommentSection(false);
   };
@@ -123,74 +126,72 @@ export default function PostContainer({ videoData }) {
   const handleCommentBtnClick = () => {
     setShowCommentSection(!showCommentSection);
   };
-  
 
   return (
     <div className="flex justify-center flex-row">
       <div className=" h-full rounded-3xl p-5 w-3/4 bg-black bg-opacity-40">
         {userData && (
           <div className="username flex p-5 text-amber-200 text-xl">
-            <img className=" rounded-full h-24" src="https://avatars.dicebear.com/api/adventurer-neutral/mail%40ashallendesign.co.uk.svg" />
+            <img
+              className=" rounded-full h-24"
+              src="https://avatars.dicebear.com/api/adventurer-neutral/mail%40ashallendesign.co.uk.svg"
+            />
             <div className="pl-4">
               <p>
-              <span className="text-3xl">{userData.firstName}</span> | <span className="font-light">{userData.bio}</span>
-            </p>
-            <p className="pt-2">{videoData.title}</p>
+                <span className="text-3xl">{userData.firstName}</span> |{" "}
+                <span className="font-light">{userData.bio}</span>
+              </p>
+              <p className="pt-2">{videoData.title}</p>
             </div>
           </div>
         )}
         <hr className="mt-2 mb-2" />
         <div className="app_videos h-full flex justify-center relative rounded-2xl overflow-scroll">
-          <Video videoData={videoData} />
-          <div className="flex flex-col-reverse">
-        {userHasLiked ? (
-          <AiFillHeart
-            className="m-4 hover:cursor-pointer"
-            style={{ color: "tan" }}
-            size={28}
-            onClick={() => {
-              console.log('videoData.id:', videoData.id); // debugging logs
-              console.log('uid:', uid);
-              unlikeVideo(videoData.id, uid); // Call the unlikeVideo function
-            }}
-          />
-        ) : (
-          <AiOutlineHeart
-            className="m-4 hover:cursor-pointer"
-            style={{ color: "tan" }}
-            size={28}
-            onClick={() => {
-              console.log('videoData.id:', videoData.id); // debugging logs
-              console.log('uid:', uid);
-              likeVideo(videoData.id, uid);
-            }}
-          />
-        )}
+          
+          <div className="flex flex-col">
+            {userHasLiked ? (
+              <AiFillHeart
+                className="m-4 hover:cursor-pointer"
+                style={{ color: "tan" }}
+                size={28}
+                onClick={() => {
+                  console.log("videoData.id:", videoData.id); // debugging logs
+                  console.log("uid:", uid);
+                  unlikeVideo(videoData.id, uid); // Call the unlikeVideo function
+                }}
+              />
+            ) : (
+              <AiOutlineHeart
+                className="m-4 hover:cursor-pointer"
+                style={{ color: "tan" }}
+                size={28}
+                onClick={() => {
+                  console.log("videoData.id:", videoData.id); // debugging logs
+                  console.log("uid:", uid);
+                  likeVideo(videoData.id, uid);
+                }}
+              />
+            )}
             <BiCommentDetail
               className="m-4 hover:cursor-pointer"
               style={{ color: "tan" }}
               size={28}
-      
-                onClick={handleCommentBtnClick}
-            
+              onClick={handleCommentBtnClick}
             />
             <BiBookmarks className="m-4" style={{ color: "tan" }} size={28} />
             <BiShare className="m-4" style={{ color: "tan" }} size={28} />
           </div>
+          <Video videoData={videoData} />
           {showCommentSection && (
-          <>
-          <CommentSection handleClose={handleCloseCommentSection} />
-          {/* fetch comments related to the video post and map them to display in this  section. */}
-          {/* Add code here to display the list of comments */}
-          {/* Add code here to display the form to submit a comment */}
-          </>
-         )}
-         </div>
-      
+            <>
+              <CommentSection handleClose={handleCloseCommentSection} />
+              {/* fetch comments related to the video post and map them to display in this  section. */}
+              {/* Add code here to display the list of comments */}
+              {/* Add code here to display the form to submit a comment */}
+            </>
+          )}
+        </div>
       </div>
     </div>
   );
 }
-
-
-

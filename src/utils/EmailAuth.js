31d17ea6db1ada8a37d25945cf50d8b2b9c6be34
abigailmgarcia <abigailmg31@gmail.com>
2023-07-25 06:52:20 -0500

    removed unused code

--- src/utils/EmailAuth.js
+++ src/utils/EmailAuth.js
@@ -1,37 +1,3 @@
-// import { getAuth, sendSignInLinkToEmail, isSignInWithEmailLink, signInWithEmailLink } from "firebase/auth";
-
-
-
-// export const actionCodeSettings = {
-//     url: 'https://project3-15aff.firebaseapp.com/__/auth/action?mode=action&oobCode=code',
-//     handleCodeInApp: true,
-//     dynamicLinkdomain: 'project3-15aff.firebaseapp.com'
-// };
-
-// const auth = getAuth();
-// sendSignInLinkToEmail(auth, email, actionCodeSettings)
-//   .then(() => {
-//     alert("Sign-up link successfully sent to your email!")
-//     //saves email locally 
-//     window.localStorage.setItem('emailForSignIn', email);
-//     // window.location.href = "/email";
-//   })
-//   .catch((error) => {
-//     const errorCode = error.code;
-//     const errorMessage = error.message;
-//   });
-
-//   if(isSignInWithEmailLink(auth, window.location.href)) {
-//     let email = window.localStorage.getItem('emailForSignIn');
-//     if (!email){
-//         email = window.prompt('Please provide your email confirmation!');
-//     }
-//     signInWithEmailLink(auth, email, window.location.href)
-//         .then((result) => {
-//             window.localStorage.removeItem('emailForSignIn');
-//         });
-//   }
-
 // ForgotPassword.js
 import React, { useState } from "react";
 import {getAuth, sendPasswordResetEmail } from "firebase/auth"

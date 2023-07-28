082831dbeefbfd772111e7330233ba18aa83fd70
Jstanion <123262263+Jstanion@users.noreply.github.com>
2023-07-27 21:56:51 -0500

    removed unneeded imports

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes, Switch, Link } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import React from "react";
 
@@ -24,6 +24,6 @@ function App() {
       </Routes>  
     </BrowserRouter>
 
-  );
-};
+  )
+}
 export default App;

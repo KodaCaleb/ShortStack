4a2605bda174b6b0d25b058403938bd459febd7c
Jstanion <123262263+Jstanion@users.noreply.github.com>
2023-07-27 19:04:29 -0500

    added comments to uploadLogic.js

--- src/components/uploadLogic/uploadLogic.js
+++ src/components/uploadLogic/uploadLogic.js
@@ -1,63 +1,81 @@
+// Import React and useState hook from 'react' package
 import React, { useState } from "react";
+// Import specific functions and objects from Firebase packages
 import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import { collection, addDoc } from "firebase/firestore";
 import { storage, firestore } from "../../firebase";
 
+// VideoUpload Component
 export default function VideoUpload() {
+  // State variables to manage the upload behavior
   const [file, setFile] = useState(null);
-  const { title, setTitle } = useState("");
-  const { uploading, setUploading } = useState(false);
+  const [title, setTitle] = useState("");
+  const [uploading, setUploading] = useState(false);
 
+  // Function to handle file selection change
   const handleFileChange = (e) => {
     setFile(e.target.files[0]);
   };
-
+  // Function to handle video upload
   const handleUpload = async (e) => {
     e.preventDefault();
+    // Check if a file is selected or if uploading is already in progress
     if (!file || uploading) return;
 
-    const storageRef = ref(storage, file.name); // create a reference in Firebase Storage
-    const uploadTask = uploadBytesResumable(storageRef, file); // start the upload task
+    // Create a reference in Firebase Storage for the selected file
+    const storageRef = ref(storage, file.name);
+    
+    // Start the upload task
+    const uploadTask = uploadBytesResumable(storageRef, file);
 
-    setUploading(true); // set uploading state to true to prevent user from uploading again
+    // Set uploading state to true to prevent user from uploading again
+    setUploading(true);
 
     // listen to state changes, errors, and completion of the upload.
     uploadTask.on(
       "state_changed",
       (snapshot) => {
-        // use this part to show progress bar
+      // Use this part to show progress bar (not implemented in the provided code)
       },
       (error) => {
         console.log("error uploading file", error);
+        // If there's an error, set uploading state back to false
         setUploading(false);
       },
       () => {
+        // Once the file is uploaded, get the download URL of the file
         getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
-          // once file is uploaded, add document to firestore
+          // Create an object with video data to be added to Firestore
           const videoData = {
             title,
             vidRef: downloadURL,
           };
 
           try {
+            // Add the videoData object to the "videos" collection in Firestore
             await addDoc(collection(firestore, "videos"), videoData);
           } catch (error) {
             console.error("Error adding document", error);
           }
+
+          // Set uploading state back to false as the upload is complete
           setUploading(false);
         });
       }
     );
   };
 
+  // Rendering the VideoUpload component
   return (
     <form onSubmit={handleUpload}>
+      {/* Input field for file selection */}
       <input
         type="file"
         onChange={handleFileChange}
         accept="video/*"
         required
       />
+      {/* Input field for video title */}
       <input
         type="text"
         value={title}
@@ -65,6 +83,7 @@ export default function VideoUpload() {
         placeholder="Title"
         required
       />
+      {/* Button to trigger the video upload */}
       <button
         type="button"
         disabled={uploading}>

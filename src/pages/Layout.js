a168846c6e8212a3c01c634a5b836815af212a77
cjsolis <cjelizondo12@gmail.com>
2023-07-27 19:19:59 -0500

    fixed spacing in layout, home, and notfound page

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,35 +1,34 @@
-import { Outlet } from "react-router-dom"
-import NavBar from "../components/header/NavBar"
-import { auth } from "../firebase"
+import { Outlet } from "react-router-dom";
+import NavBar from "../components/header/NavBar";
+import { auth } from "../firebase";
 import { onAuthStateChanged } from "firebase/auth";
 import SearchContext from "../utils/SearchContext";
 import React, { useState } from "react";
 
 export default function Layout() {
+  const [matchingVideos, setMatchingVideos] = useState([]);
 
-    const [matchingVideos, setMatchingVideos] = useState([]);
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      // User is signed in, see docs for a list of available properties
+      // https://firebase.google.com/docs/reference/js/auth.user
+      const uid = user.uid;
+    } else {
+      // User is signed out
+      // ...
+    }
+  });
 
-    onAuthStateChanged(auth, (user) => {
-        if (user) {
-            // User is signed in, see docs for a list of available properties
-            // https://firebase.google.com/docs/reference/js/auth.user
-            const uid = user.uid;
-        } else {
-            // User is signed out
-            // ...
-        };
-    });
-
-    return (
-        <>
-        <SearchContext.Provider value={{matchingVideos, setMatchingVideos}}>
+  return (
+    <>
+      <SearchContext.Provider value={{ matchingVideos, setMatchingVideos }}>
         <div className="flex flex-col">
-            <NavBar />
-            <div className="flex-grow">
-                <Outlet />
-            </div>
+          <NavBar />
+          <div className="flex-grow">
+            <Outlet />
+          </div>
         </div>
-        </SearchContext.Provider>
-        </>
-    )
+      </SearchContext.Provider>
+    </>
+  );
 }

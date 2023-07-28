53ea1108839b39ae1d51b80565d989f628ccdcd8
Jstanion <123262263+Jstanion@users.noreply.github.com>
2023-07-27 21:06:29 -0500

    added comments to file

--- src/utils/searchLogic/SearchContext.js
+++ src/utils/searchLogic/SearchContext.js
@@ -1,6 +1,7 @@
 import React from "react";
 
+// Create a new React context named "SearchContext"
 const SearchContext = React.createContext();
 
+// Export the "SearchContext" so that other components can use it
 export default SearchContext;
-

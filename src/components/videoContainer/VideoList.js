97384b63666a461d6fc81e884f703303e06d33b4
Jstanion <123262263+Jstanion@users.noreply.github.com>
2023-07-30 05:14:25 -0500

    styled load button

--- src/components/videoContainer/VideoList.js
+++ src/components/videoContainer/VideoList.js
@@ -57,25 +57,35 @@ export default function VideoList() {
   const videoList = matchingVideos.length > 0 ? matchingVideos : videos;
 
   return (
-    <div className=" h-screen rounded-2xl">
-      <div className="app_videos h-full w-full relative flex flex-col rounded-2xl overflow-scroll">
+    <div
+      className="flex justify-center h-screen rounded-2xl">
+
+      {/* Fixed black backdrop */}
+      <div className="fixed h-full rounded-3xl w-full md:w-3/4 bg-black bg-opacity-40 pointer-events-none z-5"></div>
+      <div
+        className="app_videos h-full w-full relative flex flex-col rounded-2xl overflow-scroll"
+      >
         {videoList.map((video) => (
           <PostContainer key={video.id} videoData={video} />
         ))}
+        <div className="flex justify-center z-10">
 
-        <button
-          className="text-yellow-800 text-xl mb-20 w-fit"
-          onClick={() => {
-            fetchVideos(lastDoc);
-            window.scrollTo(0, 0);
-          }}
-        >
-          <MdOutlineExpandCircleDown
-          size={120}
-          style={{color:"tan"}}
-          />
-          Load More
-        </button>
+          <button
+            className="text-yellow-800 mb-5 w-fit hover:text-yellow-500"
+            onClick={() => {
+              fetchVideos(lastDoc);
+              window.scrollTo(0, 0);
+            }}
+          >
+            Want a
+            <br></br>
+            Bigger Stack?
+            <MdOutlineExpandCircleDown
+              size={120}
+              style={{ color: "tan" }}
+            />
+          </button>
+        </div>
       </div>
     </div>
   );
